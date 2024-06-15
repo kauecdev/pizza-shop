af94005289695fc103d1e9bdd4cfe9bfd944bada
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-06-15 18:39:15 -0300

    Added unit test for sign-in page

---
+++ src/pages/auth/sign-in.spec.tsx
@@ -0,0 +1,26 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { render } from '@testing-library/react'
+import { HelmetProvider } from 'react-helmet-async'
+import { MemoryRouter } from 'react-router-dom'
+
+import { queryClient } from '@/lib/react-query'
+
+import { SignIn } from './sign-in'
+
+describe('SignIn', () => {
+  it('should set default email input value if email is present on search params', () => {
+    const wrapper = render(<SignIn />, {
+      wrapper: ({ children }) => (
+        <MemoryRouter initialEntries={['/sign-in?email=johndoe@mail.com']}>
+          <QueryClientProvider client={queryClient}>
+            <HelmetProvider>{children}</HelmetProvider>
+          </QueryClientProvider>
+        </MemoryRouter>
+      ),
+    })
+
+    const emailInput = wrapper.getByLabelText('Seu e-mail') as HTMLInputElement
+
+    expect(emailInput.value).toEqual('johndoe@mail.com')
+  })
+})
